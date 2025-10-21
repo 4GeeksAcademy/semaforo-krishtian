e7d86001b0d6898f8631854aa24c8844a54bb443
krishtian90 <krishtian90@gmail.com>
2025-10-21 01:49:04 +0000

    semaforo-krishtian

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+//create your first component
+const TrafficLight = () => {
+  const [color, setColor] = React.useState(null);
+
+  return (
+    <div className="traffic-light">
+      <div
+        className={"light red " + (color === "red" ? "glow" : "")}
+        onClick={() => setColor("red")}
+      ></div>
+
+      <div
+        className={"light yellow " + (color === "yellow" ? "glow" : "")}
+        onClick={() => setColor("yellow")}
+      ></div>
+
+      <div
+        className={"light green " + (color === "green" ? "glow" : "")}
+        onClick={() => setColor("green")}
+      ></div>
+    </div>
+  );
+};
+
+export default TrafficLight;
